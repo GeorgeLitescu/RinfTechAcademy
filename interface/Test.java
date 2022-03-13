ead473ac91afd4a7a7cf28194e2e9336050286f3
GeorgeLitescu <101198624+GeorgeLitescu@users.noreply.github.com>
2022-03-13 14:24:18 +0200

    added solution to exercise3

---
+++ interface/Test.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        GeometricObject c = new Circle(2.0);
+        GeometricObject r = new Rectangle(2.0, 4.0);
+
+        System.out.println(c);
+        System.out.println(r);
+    }
+}
